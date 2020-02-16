38e3db9e2ee69736eccc61a26c2a01ebd4350fd7
SethaReno <51383197+SethaReno@users.noreply.github.com>
2020-02-15 20:27:34 -0700

    added styles

---
+++ src/components/pages/about.js
@@ -0,0 +1,57 @@
+import React from "react";
+import profilePicture from "../../../static/assets/images/bio/honers.png";
+
+export default function() {
+  return (
+     <div className="content-page-wrapper">
+    <div
+     className="left-column"
+      style={{
+      background: "url(" + profilePicture + ") no-repeat",
+      backgroundSize: "cover",
+      backgroundPostition: "center"
+    }}
+    />
+    
+
+    <div className="right-column">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti delectus dolorum voluptate eius atque similique maiores corrupti,
+     perspiciatis veritatis nemo ea natus, repudiandae cum nam fuga laborum dignissimos ad illo. Beatae pariatur consequatur quas error recusandae 
+     eligendi perspiciatis, et exercitationem alias harum incidunt placeat repudiandae corporis! Eaque aperiam eos omnis, illo voluptatum maxime iusto, 
+     soluta, harum fuga nam id aut. Corporis quam assumenda minus, sunt earum doloremque omnis id impedit error, facilis esse soluta. Facilis, excepturi dolorum error,
+      temporibus ipsam id sequi, laudantium rem distinctio exercitationem doloribus. Culpa, eum voluptatum! Sequi distinctio minima nesciunt tempore vel obcaecati fugit vero in unde nobis,
+       excepturi, beatae, modi sunt officiis voluptates? Nulla reprehenderit, voluptatem blanditiis beatae soluta veritatis. Cumque quas quia aperiam provident.
+        Laboriosam fugit reprehenderit, quis ipsam repellendus beatae voluptas modi fugiat delectus earum autem. Ducimus sed provident autem illo obcaecati,
+         dignissimos eius rem vel illum laboriosam, quam numquam. Exercitationem, officiis dolore? Quia neque et, similique maiores veniam ducimus provident, blanditiis,
+          dignissimos quisquam libero deserunt fugiat alias beatae! Modi laboriosam recusandae, animi beatae reiciendis molestias ex ipsam magni. Magni dolores incidunt sit.
+           Autem a sint aut fugiat sequi libero ad, consectetur voluptate dolorem necessitatibus! Voluptate incidunt inventore exercitationem quam, magnam architecto enim recusandae
+            aspernatur cupiditate quas voluptatibus laudantium animi doloremque. Dolor, aperiam. Ut, placeat illo! Vero voluptates exercitationem nobis obcaecati, magnam accusamus quam molestiae 
+            quod, neque voluptatum alias asperiores ab harum incidunt a dignissimos tenetur eveniet, aspernatur adipisci? Laboriosam ut amet nemo. Pariatur autem doloribus doloremque repellat deserunt 
+            ipsum atque. Harum sunt cupiditate aspernatur, earum quas doloribus placeat autem ad animi, illum quo repellat. Quo laboriosam unde quae saepe, eligendi incidunt sapiente. Nihil aut voluptatibus 
+            incidunt voluptate, unde maxime? Totam mollitia facilis, cum eaque molestias incidunt dolor nostrum sequi assumenda explicabo laudantium eius, consequatur beatae possimus sapiente quaerat, ipsam nemo 
+            iste atque. Asperiores, labore delectus consectetur ipsam id, quas ratione quae esse vero quasi quis eaque voluptate! Quis illo ab soluta molestias mollitia accusantium enim non blanditiis, beatae consequuntur, 
+            possimus architecto odit? Aperiam harum saepe dolor, quasi consequatur adipisci. Dignissimos ea eveniet veniam earum omnis, laboriosam, quidem dolore veritatis dolorum iure temporibus
+             ipsum neque necessitatibus voluptas, iusto deleniti ipsa exercitationem nemo magni? Facilis quas obcaecati consectetur minus ea dignissimos tenetur enim eos incidunt illum. Magni facilis
+             voluptatibus cum earum, atque perspiciatis dolorum, iure laboriosam obcaecati aperiam doloremque explicabo consectetur? Ea, necessitatibus tempore! Iste accusamus, soluta saepe sequi hic
+              nostrum incidunt vel vitae. Assumenda eligendi vel sapiente possimus non porro repellendus asperiores ipsum, earum voluptate harum reprehenderit praesentium iste perspiciatis pariatur impedit! Quia. 
+              Mollitia dicta distinctio ratione! Recusandae nihil quia culpa magnam quaerat quam doloribus enim tempora sapiente temporibus dolor assumenda sequi ducimus id quae neque accusantium itaque, eum
+               tempore voluptas nobis maxime. Fugiat quisquam, repellat deleniti quaerat nulla obcaecati minus placeat, recusandae sed libero quas quo fugit vel cumque beatae maxime a, soluta iste est quis 
+               modi unde ipsa. Omnis, optio quos. Laborum repudiandae veritatis, quibusdam atque cupiditate quasi reprehenderit ex sint nemo repellat, vero sit quaerat voluptatum. Vel, labore. Minima exce
+               pturi corrupti repudiandae, corporis reiciendis modi sint a omnis ratione non. Itaque eligendi minus, rerum consequuntur hic, enim sint amet corporis libero cumque iste impedit tenetur fuga nec
+               essitatibus animi autem? Sunt facilis, ea nobis fugit sed repellat voluptates ullam id corrupti. Obcaecati deserunt, asperiores magni eius tempore temporibus soluta quo commodi perspiciati
+               s deleniti sint quis odio voluptatibus voluptate velit magnam, excepturi corporis accusamus perferendis atque eveniet accusantium. Laboriosam tempore veniam dolor! Velit commodi incidunt sit 
+               laudantium dicta omnis, reiciendis quibusdam perferendis eaque, neque recusandae exercitationem ullam quaerat. Perferendis dolorum accusantium vero veniam, impedit facere quam sapiente expl
+               icabo, omnis possimus repellat eum? Similique voluptate quaerat quis voluptatibus excepturi, autem quos veniam? Soluta, quasi laudantium distinctio fugit non animi accusamus consequatur sit,
+                possimus autem ea. Et quibusdam quam voluptatem? Unde molestias repellat vel. Praesentium ipsum voluptatem quisquam quos nobis, ea alias, accusamus perspiciatis obcaecati nam ullam modi. Cu
+                mque libero voluptatibus laborum temporibus necessitatibus tenetur reiciendis ea. Magnam provident, sint rem nobis distinctio tenetur. Excepturi cum similique officiis laudantium consequuntur, 
+                numquam perferendis iusto tempore culpa aut voluptate illum, corporis eos ut hic rerum iste cumque! Exercitationem sapiente inventore ipsum velit, qui quisquam laboriosam expedita. Itaque atq
+                ue molestiae deleniti eum provident laudantium aliquam dolores molestias iste qui, consequatur amet maiores vitae expedita. Consequatur doloremque deserunt repellendus suscipit atque? Vel quasi q
+                uo at deleniti est commodi. Amet, molestias iste. Vel, dolores. Similique enim tempora, asperiores delectus incidunt vero fugiat perspiciatis repudiandae temporibus excepturi nobis doloremque c
+                umque? Aperiam voluptate commodi perferendis sint voluptatem laudantium ratione repellat nostrum! Odio ea harum enim debitis tenetur voluptatibus possimus eos expedita quidem saepe quibusdam an
+                imi assumenda corporis illo, maxime fugit aperiam esse, dolorum ad. Molestiae veniam inventore rerum velit pariatur non! Sed libero dolor iusto nisi eaque aspernatur porro tempora illum est cupiditate
+    </div>
+  </div>
+  );
+}
+
+  
---
+++ src/components/pages/no-match.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { Link } from "react-router-dom";
+  export default function() {
+        return (
+            <div>
+               <h2>We couln't find that page</h2>
+                <Link to="/">Return to hompage</Link>
+            </div>
+        );
+    }
+  
+
